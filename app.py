d5c79b40fd831101fd018a06c0f649a7c8b4862c
rakotonirinaavosoa <rakotonirinaavosoa@gmail.com>
2026-08-18 21:49:46 +0300

    app.py

--- app.py
+++ app.py
@@ -891,7 +891,7 @@ with tab_quiz:
                 """
                 <div class="lesson">
                 <b>Question rapide</b><br><br>
-                Complète : « Nous ..... au marché demain. »
+                Complète : Nous ..... au marché demain. 
                 </div>
                 """,
                 unsafe_allow_html=True,
